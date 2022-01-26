33943da941823040ab52a901b78987272db69f31
Nilay Setiya <87846852+nilaysetiya@users.noreply.github.com>
2022-01-26 14:21:31 +1300

    Implemented basic commands

--- bot.py
+++ bot.py
@@ -1,15 +1,40 @@
-from lib2to3.pgen2 import token
+import lightbulb
 import hikari
 import Consts
 
-bot = hikari.GatewayBot(token=Consts.TOKEN)
-
-@bot.listen(hikari.GuildMessageCreateEvent)
-async def print_message(event):
-    print(event.content)
+bot =lightbulb.BotApp(
+    token=Consts.TOKEN, 
+    default_enabled_guilds=(563614006846423040)
+)
 
 @bot.listen(hikari.StartedEvent)
-async def bot_started(event):
-    print('Bot has started')
+async def on_started(event):
+    print("Bot has started")
+
+@bot.command
+@lightbulb.command('ping', 'Says pong!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
+
+@bot.command
+@lightbulb.command('group', 'This is a group')
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def my_group(ctx):
+    pass
+
+@my_group.child
+@lightbulb.command('subcommand', 'This is a subcommand')
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def subcommand(ctx):
+    await ctx.respond('I am a subcommand')
+
+@bot.command
+@lightbulb.option('num2', 'The second number', type=int)
+@lightbulb.option('num1', 'The first number', type=int)
+@lightbulb.command('add', 'Add two numbers together')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def add(ctx):
+    await ctx.respond(ctx.options.num1 + ctx.options.num2)
 
 bot.run()
